0803d086ca6e492622316929b17a6488acc313be
quan118 <quannt89@gmail.com>
2023-07-15 14:21:16 +0700

    add @react-navigation/native, react-native-screens and react-native-safe-area-context

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -12,6 +12,10 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+const sleep = (ms: number) => new Promise(resolve => setTimeout(() => resolve(true), ms));
+
+it('renders correctly', async () => {
   renderer.create(<App />);
+
+  await sleep(10); // work around for this issue: https://github.com/react-navigation/react-navigation/issues/10943
 });
