bbf4434a21801de4bfd3c41779cc095fc8d78f3a
quan118 <quannt89@gmail.com>
2023-07-15 14:41:53 +0700

    fix linting

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -12,7 +12,8 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(() => resolve(true), ms));
+const sleep = (ms: number) =>
+  new Promise(resolve => setTimeout(() => resolve(true), ms));
 
 it('renders correctly', async () => {
   renderer.create(<App />);
--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,2 @@
 // include this line for mocking react-native-gesture-handler
 require('react-native-gesture-handler/jestSetup');
-
